902e4447acf298ef5d38e27a04c4dd6da47cf150
CHUFASCHIN <jmmadridd@gmail.com>
2015-11-20 19:29:41 +0100

    commit 2 20/11/2015

--- app/src/main/java/com/example/chufaschin/mockupstallerizate/NuevoCliente.java
+++ app/src/main/java/com/example/chufaschin/mockupstallerizate/NuevoCliente.java
@@ -1,7 +1,9 @@
 package com.example.chufaschin.mockupstallerizate;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.app.Activity;
+import android.view.View;
 
 public class NuevoCliente extends Activity {
 
@@ -9,6 +11,11 @@ public class NuevoCliente extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_nuevo_cliente);
+    }
+
+    public void lanzarAdministracion(View view){
+        Intent intent= new Intent(this, Administracion.class);
+        startActivity(intent);
     }
 
 }
--- app/src/main/java/com/example/chufaschin/mockupstallerizate/NuevoVehiculo.java
+++ app/src/main/java/com/example/chufaschin/mockupstallerizate/NuevoVehiculo.java
@@ -1,7 +1,9 @@
 package com.example.chufaschin.mockupstallerizate;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.app.Activity;
+import android.view.View;
 
 public class NuevoVehiculo extends Activity {
 
@@ -10,5 +12,8 @@ public class NuevoVehiculo extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_nuevo_vehiculo);
     }
-
+    public void lanzarAdministracion(View view){
+        Intent intent= new Intent(this, Administracion.class);
+        startActivity(intent);
+    }
 }
