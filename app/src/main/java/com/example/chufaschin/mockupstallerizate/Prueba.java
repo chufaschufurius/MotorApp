8249649801d02eb23eaf7192ee11cf2597dfcd06
CHUFASCHIN <jmmadridd@gmail.com>
2015-11-24 18:00:03 +0100

    commit 6 24/11/2015 15.59 intento de merge

--- app/src/main/java/com/example/chufaschin/mockupstallerizate/Prueba.java
+++ app/src/main/java/com/example/chufaschin/mockupstallerizate/Prueba.java
@@ -3,12 +3,12 @@ package com.example.chufaschin.mockupstallerizate;
 import android.os.Bundle;
 import android.app.Activity;
 
-public class AdministracionSeleccionarMarca extends Activity {
+public class Prueba extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_administracion_seleccionar_marca);
+        setContentView(R.layout.activity_prueba);
     }
 
 }
